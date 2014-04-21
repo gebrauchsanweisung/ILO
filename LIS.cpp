f7c7d1e3d089024da40b6d4ce9923167e5763a3f
gebrauchsanweisung <gebrauchsanweisung@interia.pl>
2014-04-21 17:09:55 +0200

    Dodanie komentarzy

--- LIS.cpp
+++ LIS.cpp
@@ -1,37 +1,42 @@
-#include <cstdio>
-#include <cstring>
+//Najdłuższy podciąg rosnący (The longest increasing subsequence)
 
-const int mem = 1000001, max = 2147483647;
-int arr[mem], a[mem], n;
+#include <iostream>
+
+const int mem = 1000001, infinity = 2147483647;
+int data[mem], buildLIS[mem], n;
 
 void read() {
-    scanf( "%d", &n );
-    for (int i = 0; i < n; ++i) {
-        scanf( "%d", &arr[i] );
-        a[i] = max;
+    std::cin >> n; // Wczytaj ilość danych liczb.
+    for ( int i = 0; i < n; ++i ) {
+        std::cin >> data[i]; // Wczytaj dane liczby.
+        build[i] = infinity; // Wypełnij tablicę "budujNPR" od 0 do n (patrz:
+                             // linia 15) wartością "nieskończoną" (infinity).
     }
+    build[n] = infinity;
 }
 
-int binary_search( int b ) {
+int upperBound( int a ) { // Wyszukaj binarnie danej wartości (patrz: 34, 35).
     int l = 0, r = n, mid;
-    while (l < r) {
-        mid = (l + r) / 2;
-        if ( a[mid] < b )
+    while ( l < r ) {
+        mid = ( l + r ) / 2;
+        if ( build[mid] < a )
             l = mid + 1;
         else
             r = mid;
     }
-    return l;
+    return l; // Zwróć indeks w tablicy "budujNPR", pod którym znajduje się szukana wartość (patrz: 34, 35).
 }
 
-int alg() {
-    for (int i = 0; i < n; ++i)
-        a[ binary_search( arr[i] ) ] = arr[i];
-    return binary_search( max );
+int searchForLIS() {
+    for ( int i = 0; i < n; ++i )
+        buildLIS[upperBound( data[i] )] = data[i]; // Wstaw daną liczbę (data[i]) pod indeksem w tablicy
+                                                   // "budujNPR", gdzie znajduje się pierwsza większa bądź
+                                                   // równa wartość w stosunku do danej liczby (data[i]).
+    return upperBound( infinity ); //Zwróć długość najdłuższego podciągu rosnącego.
 }
 
 int main() {
-    read();
-    printf( "%d\n", alg() );
+    read(); // Wczytaj dane.
+    std::cout << searchForLIS() << std::endl; // Wypisz długość najdłuższego podciągu rosnącego.
     return 0;
 }
